507a8ed2e92aed2cc58834a4a48639fb0f285de1
Pol <123597761+server343@users.noreply.github.com>
2023-12-28 21:17:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+document.getElementById('send-button').addEventListener('click', function() {
+    const userInput = document.getElementById('user-input');
+    const message = userInput.value.trim();
+    if (message) {
+        appendMessage('Tú', message);
+        sendMessageToServer(message);
+        userInput.value = '';
+    }
+});
+
+function appendMessage(sender, message) {
+    const messageContainer = document.getElementById('messages');
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = `${sender}: ${message}`;
+    messageContainer.appendChild(messageDiv);
+}
+
+function sendMessageToServer(message) {
+    fetch('/send_message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: message })
+    })
+    .then(response => response.json())
+    .then(data => {
+        appendMessage('Chatbot', data.reply);
+    })
+    .catch(error => {
+        console.error('Error al enviar el mensaje:', error);
+    });
+}
 
